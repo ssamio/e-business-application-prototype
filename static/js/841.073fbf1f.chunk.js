3939a80f97369686fded360ea3f4eecf2ada9cab
ssamio <sami.o.seppala@proton.me>
2023-11-28 19:41:58 +0200

    Updates

---
+++ static/js/841.073fbf1f.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 841.073fbf1f.chunk.js.LICENSE.txt */
+"use strict";(self.webpackChunke_business_application_prototype=self.webpackChunke_business_application_prototype||[]).push([[841],{841:(e,t,s)=>{s.r(t),s.d(t,{startStatusTap:()=>i});var n=s(554),o=s(743),r=s(811);const i=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,n.wj)((()=>{const t=e.innerWidth,s=e.innerHeight,i=document.elementFromPoint(t/2,s/2);if(!i)return;const a=(0,o.a)(i);a&&new Promise((e=>(0,r.c)(a,e))).then((()=>{(0,n.Iu)((async()=>{a.style.setProperty("--overflow","hidden"),await(0,o.s)(a,300),a.style.removeProperty("--overflow")}))}))}))}))}}}]);
+//# sourceMappingURL=841.073fbf1f.chunk.js.map
