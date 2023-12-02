dd4a8939f61c01f408766b976dd214851e4e32e6
ssamio <sami.o.seppala@proton.me>
2023-12-02 22:40:42 +0200

    Markers

--- src/ScooterMap.js
+++ src/ScooterMap.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { Map } from "pigeon-maps";
+import { Map, Marker } from "pigeon-maps";
 
 const ScooterMap = () => {
   const position = [63.096, 21.615];
 
-  return <Map defaultCenter={position} defaultZoom={13}></Map>;
+  return (
+    <Map defaultCenter={position} defaultZoom={13}>
+      <Marker anchor={position} color="green"></Marker>
+      <Marker anchor={[63.09621, 21.61533]} color="green"></Marker>
+      <Marker anchor={[63.096567, 21.615897]} color="green"></Marker>
+      <Marker anchor={[63.096812, 21.615345345]} color="blue"></Marker>
+      <Marker anchor={[63.0964234234, 21.615345345]} color="red"></Marker>
+    </Map>
+  );
 };
 
 export default ScooterMap;
